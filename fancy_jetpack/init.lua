dbfb04a3b1eec3007f6eeb8bea39454fc3561d0f
nuke <nick.ratuski@gmail.com>
2019-10-30 12:59:59 -0400

    added fancy jetpack mod

---
+++ fancy_jetpack/init.lua
@@ -0,0 +1,19 @@
+
+function OnModPreInit()
+	
+end
+
+function OnModInit()
+	
+end
+
+function OnModPostInit()
+	
+end
+
+function OnPlayerSpawned( player_entity ) 
+
+end
+
+print("Fancy jetpack mod initialization done.")
+
