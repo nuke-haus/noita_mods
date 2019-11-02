bd3dd6760f0da8f1be593bad983964ae24201b57
nuke <nick.ratuski@gmail.com>
2019-11-02 14:19:49 -0400

    updated repelling field.

--- nukes_perks/files/nuke/scripts/repelling_field.lua
+++ nukes_perks/files/nuke/scripts/repelling_field.lua
@@ -1,10 +1,22 @@
 dofile( "data/scripts/lib/utilities.lua" )
 
-table.insert( perk_list, 
+local new_list = {}
+
+for k,v in pairs(perk_list) do
+
+	if (v.id ~= "PROJECTILE_REPULSION") then
+
+		table.insert(new_list, v)
+
+	end
+
+end
+
+table.insert( new_list, 
 {
 	id = "REPEL_FIELD",
 	ui_name = "Repelling Field",
-	ui_description = "An invisible field pushes objects and projectiles away from you.",
+	ui_description = "An invisible field pushes objects and projectiles away from you, but you feel more frail.",
 	ui_icon = "mods/nukes_perks/files/nuke/perks_gfx/ui/repel_field.png",
 	perk_icon = "mods/nukes_perks/files/nuke/perks_gfx/ig/repel_field.png",
 	func = function(perk_item, player, item_name)
@@ -13,6 +25,26 @@ table.insert( perk_list,
 		local child_id = EntityLoad("mods/nukes_perks/files/nuke/entities/repel_field.xml", x, y)
 		
 		EntityAddChild(player, child_id)
+
+		local damagemodels = EntityGetComponent( entity_who_picked, "DamageModelComponent" )
+		local dmg_types = {"ice", "slice", "projectile", "physics_hit", "explosion", "melee", "drill"}
+		
+		if( damagemodels ~= nil ) then
+		
+			for i,damagemodel in ipairs(damagemodels) do
+			
+				for k,dmg in pairs(dmg_types) do
+				
+					local resistance = tonumber(ComponentObjectGetValue( damagemodel, "damage_multipliers", dmg ))
+					resistance = resistance * 1.2
+					ComponentObjectSetValue( damagemodel, "damage_multipliers", dmg, tostring(resistance) )
+					
+				end
+				
+			end
+		end
 		
 	end,
 })
+
+perk_list = new_list
