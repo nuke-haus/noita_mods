6cc64c55f5e7ce8658cb6a669fd9e93206d283fc
nuke <nick.ratuski@gmail.com>
2019-11-20 12:56:01 -0500

    added improved workshop mod

---
+++ improved_workshop/files/nuke/scripts/CONFIG_IFRAMES.lua
@@ -0,0 +1,7 @@
+
+-- How many iframes should the player get when they take damage?
+NUMBER_OF_IFRAMES = 20
+
+-- If damage is between 0 and this number (inclusive), don't give the player iframes
+IFRAME_THRESHOLD = 8.0
+
---
+++ improved_workshop/init.lua
@@ -0,0 +1,21 @@
+
+function OnModPreInit()
+	
+end
+
+function OnModInit()
+	
+end
+
+function OnModPostInit()
+	
+end
+
+function OnPlayerSpawned(player) 
+
+end
+
+ModLuaFileAppend( "data/scripts/biomes/temple_altar.lua", "files/nuke/scripts/workshop.lua")
+
+print("Improved workshop mod initialization done.")
+
