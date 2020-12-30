0d28c4886798e9f4ce5933a4442174051a8256fe
nuke <nick.ratuski@gmail.com>
2020-12-30 11:58:52 -0500

    Fix peaceful shopping mod

--- nukes_peaceful_shopping/files/nuke/scripts/workshop_right.lua
+++ nukes_peaceful_shopping/files/nuke/scripts/workshop_right.lua
@@ -1,20 +1,25 @@
 
-function spawn_all_perks( x, y )
 
-	SetRandomSeed( x, y )
+function temple_spawn_guardian( pos_x, pos_y )
 
-    if ( GlobalsGetValue( "TEMPLE_SPAWN_GUARDIAN" ) == "1" ) then
-        
-        EntityLoad( "data/entities/misc/spawn_necromancer_shop.xml", x + 190, y + 110 )
-        
-    end
-
-    local spawn_shop, do_spawn_perks = temple_random( x, y )
-    
-	if( do_spawn_perks == "0" ) then
+	if( GlobalsGetValue( "TEMPLE_PEACE_WITH_GODS" ) == "1" ) then
+		-- peace_with_gods perk
 		return
 	end
-	
-    perk_spawn_many( x, y )
 
+	local guard_spawn_id = EntityGetClosestWithTag( pos_x, pos_y, "guardian_spawn_pos" )
+	local guard_x = pos_x
+	local guard_y = pos_y
+
+	-- if distance is too far
+    if( guard_spawn_id ~= 0  ) then
+        
+		guard_x, guard_y = EntityGetTransform( guard_spawn_id )
+		-- kill the spawn target, so that it doesn't come and haunt us in subsequent spawns
+        EntityKill( guard_spawn_id )
+        
+	end
+
+    EntityLoad( "data/entities/misc/spawn_necromancer_shop.xml", guard_x + 190, guard_y + 110 ) -- move him out of the temple
+    
 end
--- nukes_peaceful_shopping/init.lua
+++ nukes_peaceful_shopping/init.lua
@@ -15,9 +15,7 @@ function OnPlayerSpawned(player)
 
 end
 
-ModLuaFileAppend( "data/scripts/biomes/temple_altar_right.lua", "mods/nukes_peaceful_shopping/files/nuke/scripts/workshop_right.lua")
-ModLuaFileAppend( "data/scripts/biomes/temple_altar_right_snowcastle.lua", "mods/nukes_peaceful_shopping/files/nuke/scripts/workshop_right.lua")
-ModLuaFileAppend( "data/scripts/biomes/temple_altar_right_snowcave.lua", "mods/nukes_peaceful_shopping/files/nuke/scripts/workshop_right.lua")
+ModLuaFileAppend( "data/scripts/biomes/temple_shared.lua", "mods/nukes_peaceful_shopping/files/nuke/scripts/workshop_right.lua")
 
 print("Peaceful shopping mod initialization done.")
 
