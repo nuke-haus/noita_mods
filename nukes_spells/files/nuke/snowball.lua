83306c1dc1d479abe1c011716c8824036120fbc4
nüke <nuke.twoski@gmail.com>
2021-06-06 16:55:28 -0400

    snowball

---
+++ nukes_spells/files/nuke/snowball.lua
@@ -0,0 +1,55 @@
+
+table.insert( actions,
+{
+	id          = "SNOWBALL",
+	name 		= "Snowball",
+	description = "A freezing ball of snow",
+	sprite 		= "mods/nukes_spells/files/nuke/spell_gfx/snowball.png",
+	type 		= ACTION_TYPE_PROJECTILE,
+	spawn_level                       = "0,1,2,3", 
+	spawn_probability                 = "1,1,1,1", 
+	price = 110,
+	mana = 5,
+	action 		= function()
+		add_projectile("mods/nukes_spells/files/nuke/entities/snowball.xml")
+		c.screenshake = c.screenshake + 0.5
+		c.fire_rate_wait = c.fire_rate_wait + 4
+	end,
+} )
+
+table.insert( actions,
+{
+	id          = "SNOWBALL_TRIGGER",
+	name 		= "Snowball with trigger",
+	description = "A freezing ball of snow that casts another spell upon collision",
+	sprite 		= "mods/nukes_spells/files/nuke/spell_gfx/snowball.png",
+	type 		= ACTION_TYPE_PROJECTILE,
+	spawn_level                       = "0,1,2,3", 
+	spawn_probability                 = "1,1,1,1", 
+	price = 150,
+	mana = 10,
+	action 		= function()
+		c.fire_rate_wait = c.fire_rate_wait + 4
+		c.screenshake = c.screenshake + 0.5
+		add_projectile_trigger_hit_world("mods/nukes_spells/files/nuke/entities/snowball.xml", 1)
+	end,
+} )
+
+table.insert( actions,
+{
+	id          = "SNOWBALL_TIMER",
+	name 		= "Snowball with timer",
+	description = "A freezing ball of snow that casts another spell after a timer runs out",
+	sprite 		= "mods/nukes_spells/files/nuke/spell_gfx/snowball.png",
+	type 		= ACTION_TYPE_PROJECTILE,
+	spawn_level                       = "0,1,2,3", 
+	spawn_probability                 = "1,1,1,1", 
+	price = 150,
+	mana = 10,
+	action 		= function()
+		c.fire_rate_wait = c.fire_rate_wait + 4
+		c.screenshake = c.screenshake + 0.5
+		add_projectile_trigger_timer("mods/nukes_spells/files/nuke/entities/snowball.xml", 10, 1)
+	end,
+} )
+
--- nukes_spells/init.lua
+++ nukes_spells/init.lua
@@ -22,6 +22,7 @@ ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/nukes_spells/files/n
 ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/nukes_spells/files/nuke/cloud_chaos.lua")
 --ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/nukes_spells/files/nuke/trail_chaos.lua") this spell doesn't always work properly so i'm disabling it
 ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/nukes_spells/files/nuke/bomb_cluster.lua")
+ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/nukes_spells/files/nuke/snowball.lua")
 
 print("More spells mod initialization done.")
 
